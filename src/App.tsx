6dea4c9a76393ba94f527960ea6074d81ef34751
Ayo Solomon <ayoseunsolomon@gmail.com>
2026-01-12 10:23:57 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-500 text-white">
+      <h1 className="text-2xl font-thin text-white">
+        Vite + Tailwind 🚀
+      </h1>
+    </div>
+  )
+}
